6635c8bd7d2631a01311881cf59784dc53323faa
vedant <mulherkarvedant@gmail.com>
2024-06-15 22:31:35 +0530

    two projects

---
+++ Type2/Type2/src/App.js
@@ -0,0 +1,12 @@
+import logo from './logo.svg';
+import './App.css';
+import LoginSignup from './Components/LoginSignup/LoginSingup'
+function App() {
+  return (
+    <div className="App">
+      <LoginSignup/>
+    </div>
+  );
+}
+
+export default App;
